115e00f2f0f6874784c05f31adfe48f97faf8b36
Tamar Asashvili <asashvili@gmail.com>
2019-05-22 15:04:45 +0200

    user-router

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-
 const express = require ('express');
 const userRouter = require('./user-router')
 const app = express();
 
-app.use ('/api/users', userRouter);
+//app.use ('/api/users', userRouter);
+
+
+app.get('/api/users/:id', (req, res, next)=>{
+    console.log('[1] GET /api/users', req.params.id);
+    //res.sendStatus(200);
+    next();
+});
+
+app.get('/api/users/:id', (req, res)=>{
+    console.log('[2] GET /api/users', req.params.id);
+    res.sendStatus(200);
+});
+
+
+
 
 app.listen(1337, err=> {
     if(err)
@@ -11,15 +25,3 @@ app.listen(1337, err=> {
 
     console.log(`listening on port 1337`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
